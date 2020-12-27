1877fd26a63916bde89142459f9c8331863aebcb
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-27 20:10:30 -0300

    Creating Entities Class Equipamento

---
+++ AwesomeGym.Core/Entidades/Equipamento.cs
@@ -0,0 +1,23 @@
+using AwesomeGym.Core.Enums;
+using System;
+
+namespace AwesomeGym.Core.Entidades
+{
+    public class Equipamento
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public DateTime AdquiridoEm { get; private set; }
+        public StatusEquipamentoEnum Status { get; private set; }
+
+        protected Equipamento() { }
+        public Equipamento(string nome, string descricao, DateTime adquiridoEm)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            AdquiridoEm = adquiridoEm;
+            Status = StatusEquipamentoEnum.EmEstoque;
+        }
+    }
+}
