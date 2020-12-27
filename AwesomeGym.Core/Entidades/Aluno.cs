907808f2a8741f49b85b45b8face1a83cf3c986b
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-27 20:06:53 -0300

    Creating Entities Class Aluno

---
+++ AwesomeGym.Core/Entidades/Aluno.cs
@@ -0,0 +1,23 @@
+using AwesomeGym.Core.Enums;
+using System;
+
+namespace AwesomeGym.Core.Entidades
+{
+    public class Aluno
+    {
+        public int Id { get; private set; }
+        public string NomeCompleto { get; private set; }
+        public string ObservacoesMedicas { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public StatusAlunoEnum Status { get; private set; }
+
+        protected Aluno() { }
+        public Aluno(string nomeCompleto, string observacoesMedicas, DateTime dataNascimento, StatusAlunoEnum status)
+        {
+            NomeCompleto = nomeCompleto;
+            ObservacoesMedicas = observacoesMedicas;
+            DataNascimento = dataNascimento;
+            Status = status;
+        }
+    }
+}
