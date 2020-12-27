7f5a7c2aa8b91bad3e7870bafa0ccfac65b5f006
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-27 20:34:33 -0300

    Creating Interface Service IFileStorageService

---
+++ AwesomeGym.Core/Interfaces/Servicos/IFileStorageService.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeGym.Core.Interfaces.Servicos
+{
+    public interface IFileStorageService
+    {
+        
+    }
+}
