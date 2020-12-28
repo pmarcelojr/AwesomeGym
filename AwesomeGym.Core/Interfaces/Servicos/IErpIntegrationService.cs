f92995b823c056bd990ee81f9db222ef3d1c286c
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-27 21:58:31 -0300

    Modify Interface IErpIntegrationService including Task AdicionarPessoa

--- AwesomeGym.Core/Interfaces/Servicos/IErpIntegrationService.cs
+++ AwesomeGym.Core/Interfaces/Servicos/IErpIntegrationService.cs
@@ -1,7 +1,10 @@
+using AwesomeGym.Core.DTOs;
+using System.Threading.Tasks;
+
 namespace AwesomeGym.Core.Interfaces.Servicos
 {
     public interface IErpIntegrationService
     {
-        
+       Task AdicionarPessoa(AdicionarPessoaErpDTO pessoa);
     }
 }
