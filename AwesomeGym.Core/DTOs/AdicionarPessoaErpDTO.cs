7e944fc819580dec035773d06b161f8cc764a9cd
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-27 21:56:34 -0300

    Creating DTOs AdicionarPessoaErpDTO

---
+++ AwesomeGym.Core/DTOs/AdicionarPessoaErpDTO.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeGym.Core.DTOs
+{
+    public class AdicionarPessoaErpDTO
+    {
+        public string Nome_Completo { get; private set; }
+        public string Numero_Documento { get; private set; }
+        public AdicionarPessoaErpDTO(string nome_Completo, string numero_Documento)
+        {
+            Nome_Completo = nome_Completo;
+            Numero_Documento = numero_Documento;
+        }
+    }
+}
