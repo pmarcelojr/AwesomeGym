5c38adba897a919807edb54c4e1712979ac21df2
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-27 22:11:40 -0300

    Creating Unidade Repository inside file Persistencia

---
+++ AwesomeGym.Infrastructure/Persistencia/Repositorios/UnidadeRepository.cs
@@ -0,0 +1,17 @@
+using AwesomeGym.Core.Entidades;
+using AwesomeGym.Core.Interfaces.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace AwesomeGym.Infrastructure.Persistencia.Repositorios
+{
+    public class UnidadeRepository : IUnidadeRepository
+    {
+        public Task Add(Unidade unidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
