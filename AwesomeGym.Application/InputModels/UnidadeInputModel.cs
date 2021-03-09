4300a3008afb5fd5a6eb0d7e476e8b1ca1351118
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-03-09 13:37:37 -0300

    Creating InputModels to UnidadeInputModel

---
+++ AwesomeGym.Application/InputModels/UnidadeInputModel.cs
@@ -0,0 +1,8 @@
+namespace AwesomeGym.Application.InputModels
+{
+    public class UnidadeInputModel
+    {
+        public string Nome { get; set; }
+        public string EnderecoCompleto { get; set; }
+    }
+}
